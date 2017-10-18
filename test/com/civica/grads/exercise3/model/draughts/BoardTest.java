a3999c33683b48bb5fadd8f2dbf12dfd9245b673
Ryan-Civca <ryan.cullen@civica.co.uk>
2017-10-18 17:04:45 +0100

    Finished writing BoardTest

--- test/com/civica/grads/exercise3/model/draughts/BoardTest.java
+++ test/com/civica/grads/exercise3/model/draughts/BoardTest.java
@@ -1,20 +1,27 @@
 package com.civica.grads.exercise3.model.draughts;
 
-import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.*;
 
-import org.junit.Ignore;
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 
 public class BoardTest {
 	
 	@Test
-	@Ignore
 	public void toStringOutputExpectedText() {
 		
 		Board board = new Board(8);
-		String expected = "";
+		String expected = "Board [size=8, tiles=[[Lcom.civica.grads.exercise3.model.draughts.BoardTile;@6e5e91e4,"
+				+ " [Lcom.civica.grads.exercise3.model.draughts.BoardTile;@2cdf8d8a, "
+				+ "[Lcom.civica.grads.exercise3.model.draughts.BoardTile;@30946e09, "
+				+ "[Lcom.civica.grads.exercise3.model.draughts.BoardTile;@5cb0d902, "
+				+ "[Lcom.civica.grads.exercise3.model.draughts.BoardTile;@46fbb2c1,"
+				+ " [Lcom.civica.grads.exercise3.model.draughts.BoardTile;@1698c449,"
+				+ " [Lcom.civica.grads.exercise3.model.draughts.BoardTile;@5ef04b5,"
+				+ " [Lcom.civica.grads.exercise3.model.draughts.BoardTile;@5f4da5c3],"
+				+ " whiteCounters=[], blackCounters=[]]";
 		String actual = board.toString();
 		
 		assertEquals(expected, actual);
@@ -26,6 +33,37 @@ public class BoardTest {
 		
 		int expected = 8;
 		int actual = board.getSize();
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void getTilesExpectedValue() {
+		Board board = new Board(8);
+		
+		BoardTile[][] expected = new BoardTile[8][8];
+		BoardTile[][] actual = board.getTiles();
+		
+		
+		assertArrayEquals(expected, actual);
+	}
+	
+	@Test
+	public void getWhiteCountersExpectedValue() {
+		Board board = new Board(8);
+		
+		ArrayList<Counter> expected = new ArrayList<>();
+		ArrayList<Counter> actual = board.getWhiteCounters();
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void getBlackCountersExpectedValue() {
+		Board board = new Board(8);
+		
+		ArrayList<Counter> expected = new ArrayList<>();
+		ArrayList<Counter> actual = board.getBlackCounters();
 		
 		assertEquals(expected, actual);
 	}
