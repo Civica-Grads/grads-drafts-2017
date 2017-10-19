3abfe083fe5d9288b3c77bb5536dcb683a9c40cb
Rose-Civica <rose.nnando@civica.co.uk>
2017-10-19 10:30:58 +0100

    Made changes to Move class

--- src/com/civica/grads/exercise3/model/draughts/Move.java
+++ src/com/civica/grads/exercise3/model/draughts/Move.java
@@ -4,13 +4,46 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 import com.civica.grads.exercise3.interfaces.Describable;
+import com.civica.grads.exercise3.model.player.Player;
 
 public class Move implements Describable {
+	private  Counter counter;
+	private Player player;
+	private int[] starting;
+	private int[] afterMove;
 	
-	public Move() {
+	
+	public Move(Counter counter, Player player, int[] starting, int[] afterMove) {
+		
+		this.player= player;
+		this.counter= counter;
+		this.starting= starting;
+		this.afterMove= afterMove;						
 		
 	}
 	
+	
+	public Counter getCounter() {
+		return counter;
+	}
+
+
+	public Player getPlayer() {
+		return player;
+	}
+
+
+	public int[] getStarting() {
+		return starting;
+	}
+
+
+	public int[] getAfterMove() {
+		return afterMove;
+	}
+
+
+
 	@Override
 	public void describe(OutputStream out) throws IOException {
 		out.write(this.toString().getBytes()) ;  
