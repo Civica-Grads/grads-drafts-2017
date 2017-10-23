f8ef102d917102792b3a427956ee103056860f79
pierrepook-civica <pierre.pook@civica.co.uk>
2017-10-23 12:33:13 +0100

    [GRAD-015]created abstract makeMove logic class, and makeMovelogicDrauigths class

---
+++ src/com/civica/grads/boardgames/model/draughts/MakeMoveLogicDraughts.java
@@ -0,0 +1,37 @@
+package com.civica.grads.boardgames.model.draughts;
+
+import com.civica.grads.boardgames.model.Board;
+import com.civica.grads.boardgames.model.MakeMoveLogic;
+import com.civica.grads.boardgames.model.Position;
+
+public class MakeMoveLogicDraughts extends MakeMoveLogic {
+
+	public MakeMoveLogicDraughts(Board board, Position current, Position newPosition) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void checkForValidMove() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void checkIfCounterTaken() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void checkIfCounterTypeNeedChanging() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void createMoveRecord() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
