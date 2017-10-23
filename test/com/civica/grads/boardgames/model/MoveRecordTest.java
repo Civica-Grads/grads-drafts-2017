a9f262b6afdacd80a920a6a2622e917bdf601ac0
Will-Civica <william.sugg@civica.co.uk>
2017-10-23 17:43:06 +0100

    started MoveRecordTest

--- test/com/civica/grads/boardgames/model/MoveRecordTest.java
+++ test/com/civica/grads/boardgames/model/MoveRecordTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.civica.grads.boardgames.enums.Colour;
+import com.civica.grads.boardgames.enums.CounterType;
 import com.civica.grads.boardgames.model.MoveRecord;
 
 public class MoveRecordTest {
@@ -17,5 +19,84 @@ public class MoveRecordTest {
 //		
 //		assertEquals(expected, actual);
 	}
-
+	
+	
+	/*
+	 * tests that getPositionStart() returns the correct position object
+	 */
+	@Test
+	public void getPositionStartExpectedReturn() {
+	/*
+	MoveRecord(Position positionStart,
+				Position positionFinish,
+				Colour player, 
+				counterType, 
+				boolean counterTaken);
+	*/
+	
+		// create start and end positions
+		Position startPosition = new Position(1, 1);
+		Position finishPosition = new Position(1, 1);
+	
+		// no counter taken during move
+		boolean counterTaken = false;
+	
+		// create a moveRecord
+		MoveRecord moveRecord = new MoveRecord(startPosition,
+												finishPosition,
+												Colour.WHITE,
+												CounterType.NORMAL, 
+												counterTaken);
+	
+		assertEquals(startPosition, moveRecord.getPositionStart());
+	}
+	
+	/*
+	 * tests that getPositionFinish() returns the correct position object
+	 */
+	@Test
+	public void getPositionFinishExpectedReturn() {
+		
+		// create start and end positions
+		Position startPosition = new Position(0, 0);
+		Position finishPosition = new Position(1, 1);
+		
+		// no counter taken during turn
+		boolean counterTaken = false;
+		
+		// create a moveRecord
+		MoveRecord moveRecord = new MoveRecord(startPosition,
+												finishPosition,
+												Colour.WHITE,
+												CounterType.NORMAL, 
+												counterTaken);
+		
+		assertEquals(finishPosition, moveRecord.getPositionFinish());
+	}
+	
+	/*
+	 * tests that isCounterTaken() returns the correct value
+	 */
+	@Test
+	public void isCounterTakenExpectedReturn() {
+		
+		// create start and end positions
+		Position startPosition = new Position(0, 0);
+		Position finishPosition = new Position(1, 1);
+				
+		// no counter taken during turn
+		boolean counterTaken = false;
+				
+		// create a moveRecord
+		MoveRecord moveRecord = new MoveRecord(startPosition,
+												finishPosition,
+												Colour.WHITE,
+												CounterType.NORMAL, 
+												counterTaken);
+		
+		boolean expected = false;
+		
+		assertEquals(expected, moveRecord.isCounterTaken());
+	}
+	
 }
