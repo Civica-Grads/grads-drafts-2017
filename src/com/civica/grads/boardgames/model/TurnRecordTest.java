656ab5a8ef5085b81fb9c34cd52df23697425265
Damon-Civica <damon.payn@civica.co.uk>
2017-10-24 11:28:10 +0100

    Added comments.

--- src/com/civica/grads/boardgames/model/TurnRecordTest.java
+++ src/com/civica/grads/boardgames/model/TurnRecordTest.java
@@ -8,16 +8,23 @@ import com.civica.grads.boardgames.enums.Colour;
 import com.civica.grads.boardgames.enums.CounterType;
 
 public class TurnRecordTest {
+	
+	/**
+	 * Tests that a particular moveRecord has been added to turnRecord
+	 * @author Damon
+	 */
 
 	@Test
 	public void testAddMoveRecord() {
+		//WITH
 		Position positionStart = new Position(1,1);
 		Position positionFinish = new Position(2,2);
 		MoveRecord expected = new MoveRecord(positionStart, positionFinish, Colour.WHITE, CounterType.NORMAL, false);
+		//WHEN
 		TurnRecord turnRecord = new TurnRecord();
 		turnRecord.addMoveRecord(expected);
 		MoveRecord actual = turnRecord.getMoveRecordArrayList().get(0);
-		// expected = turn record with the move record
+		//THEN
 		assertEquals(expected, actual);
 		
 		
