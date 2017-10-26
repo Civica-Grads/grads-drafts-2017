1f3ef66a38dd234b2e6ceaf2f33cfb74114f9bff
pierrepook-civica <pierre.pook@civica.co.uk>
2017-10-26 11:15:15 +0100

    fixed test and errors in movelogic

--- src/com/civica/grads/boardgames/model/Counter.java
+++ src/com/civica/grads/boardgames/model/Counter.java
@@ -5,53 +5,55 @@ import java.io.OutputStream;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import com.civica.grads.boardgames.enums.Colour;
-import com.civica.grads.boardgames.enums.*;
+import com.civica.grads.boardgames.enums.CounterType;
 import com.civica.grads.boardgames.interfaces.Describable;
 
-
 public class Counter extends Piece implements Describable {
-	 
-	private final int key ; //Assign each counter a number (key) from 1 to (size-2)*size/2
-	
+
+	public CounterType type;
+	private final int key; // Assign each counter a number (key) from 1 to
+							// (size-2)*size/2
+
 	private static final AtomicInteger counterKeygen = new AtomicInteger();
-	
 
-    /**
-     * Constructor
-     * @param colour
-     * @param type
-     * @param key
-     */
-    public Counter(Colour colour) 
-        {
-        this(colour,CounterType.NORMAL,counterKeygen.getAndIncrement()); 
-        }
-
-    /**
-     * Constructor
-     * @param colour
-     * @param type
-     * @param key
-     */
-    public Counter(Colour colour, int key) 
-        {
-        this(colour,CounterType.NORMAL,key); 
-        }
-        
 	/**
 	 * Constructor
+	 * 
+	 * @param colour
+	 * @param type
+	 * @param key
+	 */
+	public Counter(Colour colour) {
+		this(colour, CounterType.NORMAL, counterKeygen.getAndIncrement());
+	}
+
+	/**
+	 * Constructor
+	 * 
+	 * @param colour
+	 * @param type
+	 * @param key
+	 */
+	public Counter(Colour colour, int key) {
+		this(colour, CounterType.NORMAL, key);
+	}
+
+	/**
+	 * Constructor
+	 * 
 	 * @param colour
 	 * @param type
 	 * @param key
 	 */
 	public Counter(Colour colour, CounterType type, int key) {
-		super(colour);
-		this.type = type ; 
-		this.key = key; 
+		super(colour, type);
+		this.type = type;
+		this.key = key;
 	}
 
 	/**
 	 * Returns enum counter type
+	 * 
 	 * @return NORMAL or KING
 	 */
 	@Override
@@ -59,30 +61,29 @@ public class Counter extends Piece implements Describable {
 		return type;
 	}
 
-
 	/**
 	 * Assigns enum type to counter
-	 * @param type NORMAL or KING
+	 * 
+	 * @param type
+	 *            NORMAL or KING
 	 */
 	public void setType(CounterType type) {
 		this.type = type;
 	}
-	
+
 	/**
 	 * Retrieves counter ID
+	 * 
 	 * @return key / ID
 	 */
 	public int getKey() {
 		return key;
 	}
 
-
-
 	@Override
 	public void describe(OutputStream out) throws IOException {
-		out.write(this.toString().getBytes()) ;  
+		out.write(this.toString().getBytes());
 	}
-
 
 	@Override
 	/**
@@ -92,8 +93,4 @@ public class Counter extends Piece implements Describable {
 		return "Counter [type=" + type + ", key=" + key + "]";
 	}
 
-
-
-
-	
 }
--- src/com/civica/grads/boardgames/model/Piece.java
+++ src/com/civica/grads/boardgames/model/Piece.java
@@ -1,30 +1,30 @@
 package com.civica.grads.boardgames.model;
 
 import com.civica.grads.boardgames.enums.Colour;
-import com.civica.grads.boardgames.enums.CounterType;
 
-	public abstract class Piece {
-		private final Colour colour ;
-		protected CounterType type;
-		
-        public Piece(Colour colour){
-			this.colour=colour;
-			
-		}
-		
-		
-		public Colour getColour() {
-			return colour;
-		}
+public abstract class Piece<CounterType> {
+	private final Colour colour;
+	private CounterType type;
 
+	/**
+	 * @return the type
+	 */
+	public CounterType getType() {
+		return type;
+	}
 
-		public CounterType getType() {
-			return type;
-		}
+	public Piece(Colour colour, CounterType type) {
+		this.colour = colour;
+		this.type = type;
 
+	}
 
-		public void setType(CounterType type) {
-			this.type = type;
-		}
-		
+	public Colour getColour() {
+		return colour;
+	}
+
+	public void setType(CounterType type) {
+		// TODO Auto-generated method stub
+
+	}
 }
